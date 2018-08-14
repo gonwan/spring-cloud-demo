a5f1b80fa2b6c7b728c10a20ebfadd049b64112b
Binson Qian <binson.qian@sumscope.com>
2018-08-14 16:40:08 +0800

    fix null.

--- authentication-service/src/main/java/com/gonwan/springcloud/authentication/AuthenticationApplication.java
+++ authentication-service/src/main/java/com/gonwan/springcloud/authentication/AuthenticationApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer  /* required for the controller to work */
 @RestController
 public class AuthenticationApplication {
 
