d3249e723d3364401406f687ae2015837e091112
sumscope <sumscope@gmail.com>
2018-08-21 02:40:42 +0800

    fixup actuator.

--- organization-service/src/main/java/com/gonwan/springcloud/organization/security/ResourceServerConfiguration.java
+++ organization-service/src/main/java/com/gonwan/springcloud/organization/security/ResourceServerConfiguration.java
@@ -2,10 +2,32 @@ package com.gonwan.springcloud.organization.security;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/*
+ * See: https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-Security-2.0
+ */
+@Configuration
+@Order(1)
+class ActuatorWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info", "health")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                .antMatchers("/v2/api-docs").permitAll()
+                .and()
+            .httpBasic();
+    }
+
+}
 
 @Configuration
 @EnableResourceServer
@@ -15,10 +37,8 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     public void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .requestMatchers(EndpointRequest.to("info", "/v2/api-docs", "/swagger-ui.html")).permitAll()
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
                 .antMatchers(HttpMethod.DELETE, "/v1/organizations/**").hasRole("ADMIN")
-                .antMatchers("/v1/organizations/**").hasRole("USER");
+                .anyRequest().authenticated();
     }
 
 }
