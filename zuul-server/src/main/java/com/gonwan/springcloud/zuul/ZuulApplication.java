ee99efb159c3e280a26e05f62c1e35a94eb4d6d9
gonwan <gonwan@gmail.com>
2018-12-19 09:40:22 +0800

    update config.

--- zuul-server/src/main/java/com/gonwan/springcloud/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/com/gonwan/springcloud/zuul/ZuulApplication.java
@@ -5,16 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import io.micrometer.core.annotation.Timed;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class ZuulApplication {
 
     @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Timed(value = "hello")
+    @GetMapping(value = "/hello", produces = "application/json")
+    public String hello() {
+        return "hello";
+    }
+
+    @Timed(value = "hello2")
+    @GetMapping(value = "/hello2", produces = "application/json")
+    public String hello2() {
+        return "hello2";
     }
 
     public static void main(String[] args) {
