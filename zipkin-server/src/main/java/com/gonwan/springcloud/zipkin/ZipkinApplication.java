9b744743a7761a9ea8ddb03fe3073b0ea6586926
gonwan <gonwan@gmail.com>
2020-03-11 01:20:15 +0800

    update zipkin server.

--- zipkin-server/src/main/java/com/gonwan/springcloud/zipkin/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/gonwan/springcloud/zipkin/ZipkinApplication.java
@@ -1,13 +1,34 @@
 package com.gonwan.springcloud.zipkin;
 
+import javax.sql.DataSource;
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinApplication {
+
+    /* Override definitions in ZipkinMySQLStorageConfiguration */
+    @Bean
+    public Executor mysqlExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("ZipkinMySQLStorage-");
+        executor.initialize();
+        return executor;
+    }
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource mysqlDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApplication.class, args);
