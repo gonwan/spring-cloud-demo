3dc4531d2d37109065938528dbec7cc202fbd0f3
gonwan <gonwan@gmail.com>
2020-10-13 14:21:08 +0800

    add read/write timeout to fix unreleased connections.

--- gateway-server/src/main/java/com/gonwan/springcloud/gateway/GatewayApplication.java
+++ gateway-server/src/main/java/com/gonwan/springcloud/gateway/GatewayApplication.java
@@ -1,10 +1,47 @@
 package com.gonwan.springcloud.gateway;
 
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+import reactor.netty.channel.BootstrapHandlers;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+    //@Bean
+    public ReactorResourceFactory reactorServerResourceFactory() {
+        ReactorResourceFactory reactorResourceFactory = new ReactorResourceFactory();
+        ConnectionProvider connectionProvider = ConnectionProvider.builder("webflux-server").build();
+        reactorResourceFactory.setConnectionProvider(connectionProvider);
+        reactorResourceFactory.setUseGlobalResources(false);
+        return reactorResourceFactory;
+    }
+
+    @Bean
+    public NettyServerCustomizer nettyServerCustomizer() {
+        return httpServer -> httpServer.tcpConfiguration(tcpServer -> {
+            tcpServer= tcpServer.option(ChannelOption.SO_KEEPALIVE, true);
+            /**
+             * We are modifying child handler, use doOnBind() instead of doOnConnection().
+             */
+            tcpServer = tcpServer.doOnBind(serverBootstrap ->
+                    BootstrapHandlers.updateConfiguration(serverBootstrap, "channelIdle", (connectionObserver, channel) -> {
+                        ChannelPipeline pipeline = channel.pipeline();
+                        pipeline.addLast(new ReadTimeoutHandler(10, TimeUnit.MINUTES));
+                        pipeline.addLast(new WriteTimeoutHandler(10, TimeUnit.MINUTES));
+                    }));
+            return tcpServer;
+        });
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
