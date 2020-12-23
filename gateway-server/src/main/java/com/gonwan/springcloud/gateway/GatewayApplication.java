3c4bff40d49dd5c8b5cc5919a2631b69b4437a13
gonwan <gonwan@gmail.com>
2020-12-23 10:38:13 +0800

    fix gateway api changes.

--- gateway-server/src/main/java/com/gonwan/springcloud/gateway/GatewayApplication.java
+++ gateway-server/src/main/java/com/gonwan/springcloud/gateway/GatewayApplication.java
@@ -1,35 +1,23 @@
 package com.gonwan.springcloud.gateway;
 
 import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
-import reactor.netty.channel.BootstrapHandlers;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     @Bean
     public NettyServerCustomizer nettyServerCustomizer() {
-        return httpServer -> httpServer.tcpConfiguration(tcpServer -> {
-            tcpServer = tcpServer.option(ChannelOption.SO_KEEPALIVE, true);
-            /*
-             * We are modifying child handler, use doOnBind() instead of doOnConnection().
-             */
-            tcpServer = tcpServer.doOnBind(serverBootstrap ->
-                    BootstrapHandlers.updateConfiguration(serverBootstrap, "channelIdle", (connectionObserver, channel) -> {
-                        ChannelPipeline pipeline = channel.pipeline();
-                        pipeline.addLast(new ReadTimeoutHandler(10, TimeUnit.MINUTES));
-                        pipeline.addLast(new WriteTimeoutHandler(10, TimeUnit.MINUTES));
-                    }));
-            return tcpServer;
-        });
+        return httpServer -> {
+            httpServer.childOption(ChannelOption.SO_KEEPALIVE, true);
+            httpServer.idleTimeout(Duration.ofMinutes(10));
+            return httpServer;
+        };
     }
 
     public static void main(String[] args) {
