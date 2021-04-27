372bd87425ea052f062bbec953e9391d1abdd2f0
Binhao Qian <qianbinhao@eastmoney.com>
2021-04-27 12:09:00 +0800

    adopt to spring cloud gateway 3.x behaviors.

--- gateway-server/src/main/java/com/gonwan/springcloud/gateway/filter/ResponseFilter.java
+++ gateway-server/src/main/java/com/gonwan/springcloud/gateway/filter/ResponseFilter.java
@@ -3,11 +3,10 @@ package com.gonwan.springcloud.gateway.filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.sleuth.CurrentTraceContext;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-
-import brave.Tracer;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -16,13 +15,13 @@ public class ResponseFilter implements GlobalFilter {
     private static final String CORRELATION_ID = "sc-correlation-id";
 
     @Autowired
-    private Tracer tracer;
+    private CurrentTraceContext currentTraceContext;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             ServerHttpResponse response = exchange.getResponse();
-            response.getHeaders().add(CORRELATION_ID, tracer.currentSpan().context().traceIdString());
+            response.getHeaders().add(CORRELATION_ID, currentTraceContext.context().traceId());
         }));
     }
 
