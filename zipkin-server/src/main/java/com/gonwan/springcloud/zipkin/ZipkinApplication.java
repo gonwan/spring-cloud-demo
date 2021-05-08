6ab6f1456a168f28e862d16f0dfab9089763ae79
Binhao Qian <qianbinhao@eastmoney.com>
2021-05-09 00:51:22 +0800

    remove unnecessary.

--- zipkin-server/src/main/java/com/gonwan/springcloud/zipkin/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/gonwan/springcloud/zipkin/ZipkinApplication.java
@@ -1,19 +1,16 @@
 package com.gonwan.springcloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import zipkin2.server.internal.EnableZipkinServer;
-import zipkin2.server.internal.mysql.ZipkinMySQLStorageConfiguration;
 
 import javax.sql.DataSource;
 
 @Configuration
-@AutoConfigureBefore(ZipkinMySQLStorageConfiguration.class)
 class ZipkinConfiguration {
 
     /* Override definitions in ZipkinMySQLStorageConfiguration */
