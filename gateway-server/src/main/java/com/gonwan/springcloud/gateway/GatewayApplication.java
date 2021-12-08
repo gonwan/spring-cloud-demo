0cf25fe6b4e44cf2c1d11be29b50c09f0f71e46f
Binhao Qian <qianbinhao@eastmoney.com>
2021-12-08 15:15:45 +0800

    use new netty properties.

--- gateway-server/src/main/java/com/gonwan/springcloud/gateway/GatewayApplication.java
+++ gateway-server/src/main/java/com/gonwan/springcloud/gateway/GatewayApplication.java
@@ -1,22 +1,10 @@
 package com.gonwan.springcloud.gateway;
 
-import io.netty.channel.ChannelOption;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
-import org.springframework.context.annotation.Bean;
-
-import java.time.Duration;
 
 @SpringBootApplication
 public class GatewayApplication {
-
-    @Bean
-    public NettyServerCustomizer nettyServerCustomizer() {
-        return httpServer -> httpServer
-                .childOption(ChannelOption.SO_KEEPALIVE, true)
-                .idleTimeout(Duration.ofMinutes(10));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
